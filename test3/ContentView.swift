af2a2d0beac74c76e0bb5724821bc39e9ebf1498
Karlis Cars <karlis.cars@gmail.com>
2020-10-15 14:10:45 +0300

    Initial Commit

---
+++ test3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  test3
+//
+//  Created by Karlis Cars on 15/10/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
