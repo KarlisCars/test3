54f4585cceabf285fb9b739627713ad44d0680a7
Karlis Cars <karlis.cars@gmail.com>
2020-10-15 14:36:30 +0300

    Day3 of code. Getting problems with Implementing objects.

--- test3/ContentView.swift
+++ test3/ContentView.swift
@@ -9,8 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+            /*@START_MENU_TOKEN@*/Text("Button")/*@END_MENU_TOKEN@*/
+        })
+            
     }
 }
 
